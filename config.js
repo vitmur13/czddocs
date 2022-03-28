e83c138a140fecba39b1f215f9cde25d1c36776d
vitmur13 <98639362+vitmur13@users.noreply.github.com>
2022-03-28 18:23:39 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  api_base: 'https://felixbot.ru/api/integration/dzplugin',
+  api_token: '101|de14dfb6132cdb5930725e0a8b952902e0d2af5d574c7a51eccaeb43c5480449',
+  timeoutUpdate: 30000,
+  discordToken: 'OTU4MDIwMDUwMzU3NDczMjkw.YkHPsw.aEdqAiVg4HklzV-hZ_g2uukzQ-s'
+}
