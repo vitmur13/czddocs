d13cf38f260cae87baf079b41dfb96483f83d21e
vitmur13 <98639362+vitmur13@users.noreply.github.com>
2022-03-28 18:10:52 +0300

    Delete 101.EGsfJa6uXL directory

--- 101.EGsfJa6uXL/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  api_base: 'https://felixbot.ru/api/integration/dzplugin',
-  api_token: '101|de14dfb6132cdb5930725e0a8b952902e0d2af5d574c7a51eccaeb43c5480449',
-  timeoutUpdate: 30000,
-  discordToken: ''
-}
